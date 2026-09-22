57f398e2a576eb3b293983162f9d401d86ce29bb
Akay729 <alecmularoni@gmail.com>
2026-09-23 00:04:33 +0200

    End Of Lesson BT Services: Distance To Target

---
+++ ActionRougelike/Source/ActionRougelike/AI/RogueBTService_CheckRangeTo.h
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTService.h"
+#include "RogueBTService_CheckRangeTo.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ACTIONROUGELIKE_API URogueBTService_CheckRangeTo : public UBTService
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(EditAnywhere, Category="AI")
+	FBlackboardKeySelector TargetActorKey;
+	
+	UPROPERTY(EditAnywhere, Category="AI")
+	FBlackboardKeySelector WithinRangeKey;
+	
+	UPROPERTY(EditDefaultsOnly, Category="AI")
+	float MaxAttackRange =  300.f;
+	
+	virtual void TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
+};
